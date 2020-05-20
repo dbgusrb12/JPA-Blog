5e478718fa2bca500b115956c3290f2687f0f00d
dbgusrb12 <yhg8484@naver.com>
2020-05-20 17:54:55 +0900

    project init

---
+++ src/test/java/com/jpa/jpaBlog/JpaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.jpaBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
